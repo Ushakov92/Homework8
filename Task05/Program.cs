c0669467ee1b8e5dcaefc316a46e7c1736cb3823
Ushakov Ilya <Riz4rd@gmail.com>
2022-12-12 17:11:47 +0500

    Add Homework8 (54,56,58,60,62)

---
+++ Task05/Program.cs
@@ -0,0 +1,65 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],2}, ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] ChangeRowsMatrix(int[,] matrix)
+{
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                for (int k = 0; k < matrix.GetLength(1); k++)
+                {
+                    if (j < k)
+                    {
+                        int temp = matrix[i, j];
+                        matrix[i, j] = matrix[i, k];
+                        matrix[i, k] = temp;
+                    }
+                }
+            }
+        }
+        return matrix;
+    }
+}
+int[,] matrix = CreateMatrixRndInt(4, 4, 1, 21);
+PrintMatrix(matrix);
+Console.WriteLine(@"Результат");
+int[,] changeRowsMatrix = ChangeRowsMatrix(matrix);
+PrintMatrix(changeRowsMatrix);
+
+
